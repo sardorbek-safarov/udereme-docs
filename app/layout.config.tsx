faf1b9e754386f07282fb947f51dca3e38e9c15e
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-08-18 11:01:30 +0500

    Initial commit ✨

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,23 +1,23 @@
-import { type DocsLayoutProps } from 'fumadocs-ui/layout';
-import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
-import { pageTree } from '@/app/source';
+import { pageTree } from '@/app/source'
+import { type HomeLayoutProps } from 'fumadocs-ui/home-layout'
+import { type DocsLayoutProps } from 'fumadocs-ui/layout'
 
 // shared configuration
 export const baseOptions: HomeLayoutProps = {
-  nav: {
-    title: 'My App',
-  },
-  links: [
-    {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
-    },
-  ],
-};
+	nav: {
+		title: 'My App',
+	},
+	links: [
+		{
+			text: 'Documentation',
+			url: '/docs',
+			active: 'nested-url',
+		},
+	],
+}
 
 // docs layout configuration
 export const docsOptions: DocsLayoutProps = {
-  ...baseOptions,
-  tree: pageTree,
-};
+	...baseOptions,
+	tree: pageTree,
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
-
-const inter = Inter({
-  subsets: ['latin'],
-});
+import { RootProvider } from 'fumadocs-ui/provider'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
+import type { ReactNode } from 'react'
+import './global.css'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body>
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang='en'
+			className={`${GeistSans.variable} ${GeistMono.variable}`}
+			suppressHydrationWarning
+		>
+			<body>
+				<RootProvider>{children}</RootProvider>
+			</body>
+		</html>
+	)
 }
